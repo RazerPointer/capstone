e343ffd146fabeed30f3a86939211b8fb0d88922
name <jinho0115556@gmail.com>
2023-05-23 16:32:25 +0900

    검색기능완료

--- js/schoolapi.js
+++ js/schoolapi.js
@@ -26,7 +26,7 @@ fetch(apiURL + "?" + new URLSearchParams(params))
     var universityName = [contentList.length];
     var universityCode = [contentList.length];
     var campusName = [contentList.length];
-
+    var schoolnamefield = document.getElementsByClassName("schoolnamefield");
 
     for (var i = 0; i < contentList.length; i++) {
       universityName[i] = contentList[i].getElementsByTagName("schoolName")[0].textContent;
@@ -41,10 +41,32 @@ fetch(apiURL + "?" + new URLSearchParams(params))
     for (var i = 0; i < contentList.length; i++) {
       var newDiv = document.createElement('div');
       var newText = document.createTextNode(universityName[i] + "(" + campusName[i] + ")");
-      var obj = document.getElementById("Input");
+      var obj = document.getElementById("schoolname");
       newDiv.appendChild(newText);
+      newDiv.className = 'schoolnamefield'
       obj.appendChild(newDiv);
+      newDiv.style.display = 'none';
     }
+
+    function filter(){
+      var search = document.getElementsByClassName("search")[0].value;
+      for (var i = 0; i < schoolnamefield.length; i++){
+        if(universityName[i].indexOf(search) != -1){
+          schoolnamefield[i].style.display = 'flex';
+        } else {
+          schoolnamefield[i].style.display = 'none';
+        }
+      }
+    }
+
+
+    var sname = document.getElementById("search");
+    sname.addEventListener('keyup',filter);
+
+
+    console.log(schoolnamefield[0]);
+
+    
   })
   .catch(function(error) {
     console.error("API 요청 중 오류가 발생했습니다.", error);
