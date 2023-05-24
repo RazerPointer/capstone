b924fc7bf2ba0f4155af21c01e95cb830d903657
razerp <jinho0115556@gmail.com>
2023-05-24 11:48:52 +0900

    검색기능구현 완성

--- js/schoolapi.js
+++ js/schoolapi.js
@@ -27,6 +27,7 @@ fetch(apiURL + "?" + new URLSearchParams(params))
     var universityCode = [contentList.length];
     var campusName = [contentList.length];
     var schoolnamefield = document.getElementsByClassName("schoolnamefield");
+    var focus = true;
 
     for (var i = 0; i < contentList.length; i++) {
       universityName[i] = contentList[i].getElementsByTagName("schoolName")[0].textContent;
@@ -43,9 +44,15 @@ fetch(apiURL + "?" + new URLSearchParams(params))
       var newText = document.createTextNode(universityName[i] + "(" + campusName[i] + ")");
       var obj = document.getElementById("schoolname");
       newDiv.appendChild(newText);
-      newDiv.className = 'schoolnamefield'
+      newDiv.className = 'schoolnamefield';
+      newDiv.id = 'schoolnamefield';
+      newDiv.href = '#';
       obj.appendChild(newDiv);
       newDiv.style.display = 'none';
+
+      //검색내용 클릭시 실행되는 이벤트 리스너
+      newDiv.addEventListener('click',copycontent);
+      newDiv.addEventListener('click',hiddencontent);
     }
 
     //검색어 필터 함수
@@ -60,10 +67,37 @@ fetch(apiURL + "?" + new URLSearchParams(params))
       }
     }
 
+    //클릭 했을때 해당내용이 검색창에 입력하는 함수
+    function copycontent() {
+      var targetvalue = event.target.textContent;
+      console.log(targetvalue);
+      var input = document.getElementById("search");
+      input.value = targetvalue;
+    }
+
+    //클릭 했을때 검색결과를 히든시키는 함수
+    function hiddencontent() {
+      for (var i = 0; i < schoolnamefield.length; i++){
+        schoolnamefield[i].style.display = 'none';
+        console.log("히든");
+      }
+      focus=false;
+    }
+
+    //focus 됐을시에 사용됨 클릭해서 입력했던 내용 지우고 처음부터 검색
+    function handlefocus() {
+      if(focus == false){
+        var input = document.getElementById("search");
+        input.value = "";
+        focus = true;
+      }
+    }
+
     //검색 이벤트 리스너 --> 실제 검색시 실행되는 부분
     var sname = document.getElementById("search");
     sname.addEventListener('keyup',filter);
-
+    sname.addEventListener('focus',handlefocus);
+  
   })
   .catch(function(error) {
     console.error("API 요청 중 오류가 발생했습니다.", error);
