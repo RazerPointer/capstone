3e11f8514ab7773ae3a5681e538bf3d60c88dfb4
name <jinho0115556@gmail.com>
2023-05-29 18:17:20 +0900

    메인페이지 상단메뉴 완성

--- js/main.js
+++ js/main.js
@@ -15,3 +15,14 @@ searchInputEl.addEventListener('blur', function(){
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder','');
 });
+
+const maincategory = document.getElementsByClassName('main_category_mypage')[0];
+
+maincategory.addEventListener('mouseover', function(){
+  document.getElementsByClassName("sub_category")[0].style.display="flex";  
+})
+
+
+maincategory.addEventListener('mouseout', function(){
+  document.getElementsByClassName("sub_category")[0].style.display="none";  
+})
