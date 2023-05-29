7c8a9d774b3bb262243ad7402649b3b507d9f39f
name <jinho0115556@gmail.com>
2023-05-29 19:39:13 +0900

    헤더 기능관련 수정 및 바디와 연동 테스트

--- js/main.js
+++ js/main.js
@@ -17,12 +17,21 @@ searchInputEl.addEventListener('blur', function(){
 });
 
 const maincategory = document.getElementsByClassName('main_category_mypage')[0];
+const subcategory = document.querySelector('.sub_category'); 
+const wrapsubcategory = document.querySelector('.wrap_sub_category');
+const mainframe = document.querySelector('.mainframe');
 
 maincategory.addEventListener('mouseover', function(){
-  document.getElementsByClassName("sub_category")[0].style.display="flex";  
-})
+  subcategory.classList.remove('hide');
+  wrapsubcategory.style.display = "flex";
+  mainframe.style.marginTop = "59px";
+  mainframe.style.transition = "margin .4s";
+});
 
 
 maincategory.addEventListener('mouseout', function(){
-  document.getElementsByClassName("sub_category")[0].style.display="none";  
-})
+  subcategory.classList.add('hide');
+  wrapsubcategory.style.display = "none";
+  mainframe.style.marginTop = "0px";
+  mainframe.style.transition = "margin .4s";
+});
